267f133ae14da1efec2dc55a8eac2c0140f85ade
Sneom <131665880+Sneom@users.noreply.github.com>
2024-01-14 17:03:08 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: AuthPage());
+    return MaterialApp(
+      home: AuthPage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
