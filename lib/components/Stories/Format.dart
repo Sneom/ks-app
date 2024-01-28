c42126bb83ae817e239050a155a802b0398fc83c
Sneom <131665880+Sneom@users.noreply.github.com>
2024-01-28 13:09:36 +0530

    Add files via upload
    stories added

---
+++ lib/components/Stories/Format.dart
@@ -0,0 +1,21 @@
+class FarmersSuccessStories {
+  final String id;
+  final String name;
+  final String imageURL;
+  final String description;
+  final String location;
+  final List<String> galleryImages;
+  int likes;
+  int views;
+
+  FarmersSuccessStories({
+    required this.id,
+    required this.name,
+    required this.imageURL,
+    required this.description,
+    required this.location,
+    required this.galleryImages,
+    this.likes = 0,
+    this.views = 0,
+  });
+}
